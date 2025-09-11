f25edcaa4a1e7a7e03391cf6c5dd4eb1d8c1dadd
heagenb03 <heagenbell@outlook.com>
2025-09-11 14:10:34 -0500

    have the multiplication table be from a 1x1 to user_number x user_number

--- php_multiplication_page.php
+++ php_multiplication_page.php
@@ -6,17 +6,29 @@
     <body>
         <h1>Multiplication Table</h1>
         <form method="post" action="">
-            Enter a number: <input type="number" name="number" required>
+            Enter a number: <input type="user_number" name="number" required>
             <input type="submit" value="Generate Table">
         </form>
         <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $number = intval($_POST['number']);
-                echo "<h2>Multiplication Table for $number</h2>";
+                $user_number = intval($_POST['user_number']);
+                echo "<h2>Multiplication Table from 1x1 to {$user_number}x{$user_number}</h2>";
                 echo "<table border='1'>";
-                for ($i = 1; $i <= 10; $i++) {
-                    $result = $number * $i;
-                    echo "<tr><td>$number x $i</td><td>$result</td></tr>";
+                
+                echo "<th'> </th>";
+                for ($num = 1; $num <= $user_number; $num++) {
+                    echo "<th'>$num</th>";
+                }
+                echo "</tr>";
+
+                for ($row = 1; $row <= $user_number; $row++) {
+                    echo "<tr>";
+                    echo "<th>$row</th>";
+                    for ($col = 1; $col <= $user_number; $col++) {
+                        $result = $row * $col;
+                        echo "<td>$result</td>";
+                    }
+                    echo "</tr>";
                 }
                 echo "</table>";
             }
